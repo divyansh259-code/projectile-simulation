da740ad3b140c328458965e422b1bee3f590a1f7
div <divyanshagrawal768@gmail.com>
2026-07-18 17:23:49 +0530

    refresh

--- project.py
+++ project.py
@@ -1,41 +1,33 @@
 import numpy as np
 
-def trajectory(v0, angle_degrees, drag_coeff=0.1, time_max=5, steps=200):
+def trajectory(v0, theta, dragcoeff=0.1, dt=0.01):
     g = 9.8
-    anglerad = np.radians(angle_degrees)
-    
-    vx0 = v0 * np.cos(anglerad)
-    vy0 = v0 * np.sin(anglerad)
-    
-    t = np.linspace(0, time_max, steps)
-    
-    xideal = vx0 * t
-    yideal = vy0 * t - 0.5 * g * t**2
-    yideal = np.maximum(0, yideal) 
-    
-    dt = t[1] - t[0]
-    xreal = [0.0]
-    yreal = [0.0]
-    vx = vx0
-    vy = vy0
+    theta_rad = np.radians(theta) #angle converted for numpy
+    vx0 = v0 * np.cos(theta_rad)
+    vy0 = v0 * np.sin(theta_rad) #def vx n vy initially
 
-    for i in range(1, len(t)):
-        ax = -drag_coeff * vx
-        ay = -g - drag_coeff * vy
-        
+    x = [0]
+    y = [0]           #real x, y and t
+    t = [0]
+    vx, vy = vx0, vy0   #real vx and vy equal to defined vx0 and vy0 initially
+    tcurrent = 0        #t at 0
+
+    while y[-1] >= 0: #while y is above ground
+        ax = -dragcoeff * vx
+        ay = -g - dragcoeff * vy
         vx += ax * dt
-        vy += ay * dt
-        
-        posx = xreal[-1] + vx * dt
-        posy = yreal[-1] + vy * dt
-        
-        if posy < 0:
-            posx = xreal[-1]
-            posy = 0.0
-            vx = 0.0
-            vy = 0.0
-            
-        xreal.append(posx)
-        yreal.append(posy)
-        
-    return t, xideal, yideal, np.array(xreal), np.array(yreal)
+        vy += ay * dt   #updated the velocity
+        tcurrent += dt      
+
+        x.append(x[-1] + vx * dt)       #updated the position
+        y.append(y[-1] + vy * dt)
+        t.append(tcurrent)      
+
+    x = np.array(x) #for plotting
+    y = np.array(y)
+    t = np.array(t)
+
+    xideal = vx0*t
+    yideal = vy0*t - 0.5*g*t**2 #ideal w/o drag
+
+    return x, y, t, xideal, yideal
